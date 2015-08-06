3c8f0ea1fc8131d9839b573c6970367bbe4a883f
mic <mic@local.com>
2015-08-06 22:32:26 +1000

    current page

--- dist/javascript/FileFilter.comb.js
+++ dist/javascript/FileFilter.comb.js
@@ -103,10 +103,10 @@
 
 })();;(function(){
 	"use script";
-	angular.module(APP.MODULE.COMMON).config([config]);
+	angular.module(APP.MODULE.COMMON).config(['$compileProvider',config]);
 
-	function config(){
-
+	function config($compileProvider){
+		//$compileProvider.debugInfoEnabled(false);
 	};
 })();;(function(){
 	"use strict"
@@ -757,6 +757,42 @@
 			}
 		};
 	};
+})();;(function(){
+	"use strict";
+	angular.module(APP.MODULE.NAV).directive("pagingSlider",['pageView','$log',pagingSlider]);
+
+	function pagingSlider(pageView,$log){
+		return {
+			restrict : 'A',
+			link: psLink
+		}
+
+		function psLink($scope, $element, $attrs){
+			$scope.currentPage = 1;
+			$element.bind("change",function(evt){
+				pageView.model.currentPage = $scope.currentPage;
+				$scope.$apply();
+			});
+
+			$element.bind("DOMMouseScroll mousewheel onmousewheel",function(evt){
+				evt.preventDefault();
+				if(evt.wheelDelta > 0 && $scope.currentPage > 1){
+					//scroll up to got to prev page (further up the screen)
+					pageView.model.currentPage = ($scope.currentPage--);
+					$scope.$apply();
+					
+				}else if (evt.wheelDelta < 0 && $scope.currentPage < pageView.model.totalPages){
+					// scroll down to go to next page  (further down the screen)
+					pageView.model.currentPage = ($scope.currentPage++);
+					$scope.$apply();
+				}
+				// pageView.model.currentPage = $scope.currentPage;
+				
+			});
+
+		};
+	};
+
 })();;(function(){
 	"use strict";
 	
--- javascript/common/config.js
+++ javascript/common/config.js
@@ -1,8 +1,8 @@
 (function(){
 	"use script";
-	angular.module(APP.MODULE.COMMON).config([config]);
+	angular.module(APP.MODULE.COMMON).config(['$compileProvider',config]);
 
-	function config(){
-
+	function config($compileProvider){
+		//$compileProvider.debugInfoEnabled(false);
 	};
 })();
