8f3793522fb6387b438dfa186a0deb95c2ebd0fe
Michael Herrmann <michael@srcinnovations.com.au>
2019-06-03 12:16:21 +1000

    drag drops

--- src/components/file-loader/file-loader.js
+++ src/components/file-loader/file-loader.js
@@ -1,10 +1,49 @@
-import React,{Component} from 'react';
-import './file-loader.css';
+import React, { Component } from "react";
+import "./file-loader.css";
 export class FileLoader extends Component {
-  
-    render() {
-      return  <div class="file-load-wrapper">
-                <div class="file-load">Drop File</div>
-              </div>
+  constructor() {
+    super();
+    this.dropRef = React.createRef();
+  }
+
+  handleDrag(e) {
+    console.log(e);
+  }
+  handleDragIn(e) {
+    console.log(e);
+  }
+  handleDragOut(e) {
+    console.log(e);
+  }
+  handleDrop(e) {
+    console.log(e);
+  }
+
+  componentDidMount() {
+    let div = this.dropRef.current;
+    if (div) {
+      div.addEventListener("dragenter", this.handleDragIn);
+      div.addEventListener("dragleave", this.handleDragOut);
+      div.addEventListener("dragover", this.handleDrag);
+      div.addEventListener("drop", this.handleDrop);
     }
+  }
+
+  componentWillUnmount() {
+    let div = this.dropRef.current;
+    if (div) {
+      div.removeEventListener("dragenter", this.handleDragIn);
+      div.removeEventListener("dragleave", this.handleDragOut);
+      div.removeEventListener("dragover", this.handleDrag);
+      div.removeEventListener("drop", this.handleDrop);
+    }
+  }
+
+  render() {
+    return (
+      <div class="file-load-wrapper" ref={this.dropRef}>
+        <div class="file-load">Drop File</div>
+      </div>
+    );
+  }
 }
